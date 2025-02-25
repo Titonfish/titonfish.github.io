5e73a791eb113ebb5ad1a63ce7bf9a827e1799a5
Charlie Polonus <capolonus@gmail.com>
2025-02-25 00:35:12 -0600

    more tests

---
+++ Scripts/ProjectGrid.js
@@ -0,0 +1,75 @@
+let projectGrid = document.getElementById("projectGrid");
+let submitButton = document.getElementById("Submit");
+let sortOrder = document.getElementById("SortOrder");
+let showGames = document.getElementById("games");
+let showVideos = document.getElementById("videos");
+
+let gridReversed = false;
+let doShowGames = true;
+let doShowVideos = true;
+
+LoadWebpage();
+
+function LoadWebpage(){
+    Promise.all([
+        fetch("Files/projectGrid.txt").then(x => x.text()),
+      ]).then(([gridData]) => UpdateProjectGrid(gridData));
+    ;
+}
+
+function UpdateProjectGrid(data){
+    let fileLines = data.split("\n");
+
+    if (gridReversed){
+        for (let i=fileLines.length-1;i>=0;i--){
+            if (fileLines[i].split("\t")[0] == "game" && !showGames
+        || fileLines[i].split("\t")[0] == "video" && !showVideos){
+            continue;
+        }
+
+            let params = fileLines[i].split("\t")[1];
+            let image = fileLines[i].split("\t")[2];
+            let title = fileLines[i].split("\t")[3];
+            let date = fileLines[i].split("\t")[4];
+            let description = fileLines[i].split("\t")[5];
+    
+            projectGrid.innerHTML += '<div class="itemLink" onclick="location.href=' + "'project.html?file="
+            + params + "';" + '"><img src="Images/'
+            + image + '.png" width="350px" height="196.875px" style="margin: 0px;" class=""><h1>'
+            + title + '</h1><p>'
+            + date + '</p><p>'
+            + description + '</p></div>';
+        }
+    }
+    else{
+        for (let i=0;i<fileLines.length;i++){
+            if (fileLines[i].split("\t")[0] == "game" && !showGames
+        || fileLines[i].split("\t")[0] == "video" && !showVideos){
+            continue;
+        }
+
+            let params = fileLines[i].split("\t")[1];
+            let image = fileLines[i].split("\t")[2];
+            let title = fileLines[i].split("\t")[3];
+            let date = fileLines[i].split("\t")[4];
+            let description = fileLines[i].split("\t")[5];
+    
+            projectGrid.innerHTML += '<div class="itemLink" onclick="location.href=' + "'project.html?file="
+            + params + "';" + '"><img src="Images/'
+            + image + '.png" width="350px" height="196.875px" style="margin: 0px;" class=""><h1>'
+            + title + '</h1><p>'
+            + date + '</p><p>'
+            + description + '</p></div>';
+        }
+    }
+    
+}
+
+submitButton.onclick = function(){
+    projectGrid.innerHTML = "";
+    gridReversed = sortOrder.options[sortOrder.selectedIndex].value == "oldnew";
+    showGames = doShowGames.checked;
+    showVideos = doShowVideos.checked;
+
+    LoadWebpage();
+}

33e6b8acdc37a724c13a642f602aeda81aad792a
Charlie Polonus <capolonus@gmail.com>
2025-10-20 17:47:03 -0400

    page test

---
+++ Scripts/PageLoader.js
@@ -0,0 +1,33 @@
+let urlParams = document.getElementById("Params");
+
+const params = new URLSearchParams(window.location.search);
+
+let pageNumber = 0;
+for (const [key, value] of params){
+    if (key == "page")
+    {
+        pageNumber = value;
+    }
+}
+
+let fileData = "";
+
+Promise.all([
+    fetch("Files/Pages/page" + pageNumber + ".txt").then(x => x.text()),
+  ]).then(([fileData]) => LoadWebpage(fileData));
+;
+
+function LoadWebpage(data){
+    let tabText = document.getElementById("tabText");
+    let titleText = document.getElementById("titleText");
+    let bodyText = document.getElementById("bodyText");
+
+    let fileLines = data.split("\n");
+
+    tabText.innerHTML = fileLines[0];
+    titleText.innerHTML = fileLines[0];
+
+    for (let i=1;i<fileLines.length;i++){
+        bodyText.innerHTML += fileLines[i] + "<br>";
+    }
+}

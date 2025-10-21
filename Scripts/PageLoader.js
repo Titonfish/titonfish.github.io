2bdd72db9d0ad19d38ff851be9bd223b9feb164c
Charlie Polonus <capolonus@gmail.com>
2025-10-20 22:21:27 -0400

    inventory test 3

--- Scripts/PageLoader.js
+++ Scripts/PageLoader.js
@@ -3,7 +3,7 @@ let urlParams = document.getElementById("Params");
 const params = new URLSearchParams(window.location.search);
 
 let pageNumber = 0;
-let inventory;
+let inventory = [];
 for (const [key, value] of params){
     if (key == "page")
     {
@@ -11,7 +11,7 @@ for (const [key, value] of params){
     }
     if (key == "inventory")
     {
-        inventory = Hex2Bin(value);
+        inventory = Hex2bin(value);
     }
 }
 
@@ -30,16 +30,16 @@ function LoadWebpage(data){
     let fileLines = data.split("\n");
 
     tabText.innerHTML = fileLines[0];
-    titleText.innerHTML = fileLines[0] + inventory;
+    titleText.innerHTML = fileLines[0];
 
     for (let i=1;i<fileLines.length;i++){
         let currentLine = fileLines[i];
         currentLine.replaceAll("\t","     ");
 
         if (currentLine.includes("/option")) {
-            let text = currentLine.split(" /option ");
+            let text = currentLine.split(" /link ");
             bodyText.innerHTML += text[0];
-            bodyText.innerHTML += '<a href="' + text[1] + '">Home</a>';
+            bodyText.innerHTML += '<a href="' + text[1] + '">Home</a><br>';
         }
         else {
             bodyText.innerHTML += fileLines[i] + "<br>";
@@ -47,12 +47,30 @@ function LoadWebpage(data){
     }
 }
 
-function checkHex(n) {
-    return/^[0-9A-Fa-f]{1,64}$/.test(n)
-}
+function Hex2bin(hex){
+    hex = hex.replace("0x", "").toLowerCase();
+    var out = "";
+    for(var c of hex) {
+        switch(c) {
+            case '0': out += "0000"; break;
+            case '1': out += "0001"; break;
+            case '2': out += "0010"; break;
+            case '3': out += "0011"; break;
+            case '4': out += "0100"; break;
+            case '5': out += "0101"; break;
+            case '6': out += "0110"; break;
+            case '7': out += "0111"; break;
+            case '8': out += "1000"; break;
+            case '9': out += "1001"; break;
+            case 'a': out += "1010"; break;
+            case 'b': out += "1011"; break;
+            case 'c': out += "1100"; break;
+            case 'd': out += "1101"; break;
+            case 'e': out += "1110"; break;
+            case 'f': out += "1111"; break;
+            default: return "";
+        }
+    }
 
-function Hex2Bin(n) {
-    if (!checkHex(n))
-        return 0;
-    return parseInt(n,16).toString(2)
+    return out;
 }

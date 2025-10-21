677d02e66d337e06587998f8285c9d9f66c70423
Charlie Polonus <capolonus@gmail.com>
2025-10-20 23:51:56 -0400

    the beauty of &nbsp;

--- Scripts/PageLoader.js
+++ Scripts/PageLoader.js
@@ -34,6 +34,7 @@ function LoadWebpage(data){
 
     for (let i=1;i<fileLines.length;i++){
         let currentLine = fileLines[i];
+        currentLine = currentLine.replace("\\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
 
         if (currentLine.includes("/option")) {
             let text = currentLine.split(" /option ");
@@ -41,7 +42,7 @@ function LoadWebpage(data){
             bodyText.innerHTML += '<a href="' + text[1] + '">Home</a><br>';
         }
         else {
-            bodyText.innerHTML += "     " + currentLine + "<br>";
+            bodyText.innerHTML += currentLine + "<br>";
         }
     }
 }
